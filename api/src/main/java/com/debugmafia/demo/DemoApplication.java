a18745e38a770927a3f8d8418655b5cf185b74a9
greenca6 <greenca6@gmail.com>
2018-09-30 23:41:33 -0400

    linting

--- api/src/main/java/com/debugmafia/demo/DemoApplication.java
+++ api/src/main/java/com/debugmafia/demo/DemoApplication.java
@@ -10,31 +10,30 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-
 import java.util.*;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-	
-	@RequestMapping("/api/test")
-	public Map<String, String> hello() {
-		HashMap<String, String> map = new HashMap<>();
-		map.put("test", "hello!!!");
-		return map;
-	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  @RequestMapping("/api/test")
+  public Map<String, String> hello() {
+    HashMap<String, String> map = new HashMap<>();
+    map.put("test", "hello!!!");
+    return map;
+  }
 
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**/*").allowedOrigins("*");
-            }
-        };
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**/*").allowedOrigins("*");
+      }
+    };
+  }
 }
