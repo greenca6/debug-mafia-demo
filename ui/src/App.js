d5f0c91fb6d1e606b669ea0501604110e5988118
Sweethezus <43282524+Sweethezus@users.noreply.github.com>
2018-10-13 16:08:33 -0400

    Websocket (initial)
    Initial commit of websockt demo from Spring tutorial

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import SockJS from 'sockjs-client';
+import * as Stomp from 'stomp-websocket';
 import axios from 'axios';
 import './App.css';
+
+var stompClient = null;
+var serverMessage = null;
 
 class App extends Component {
   state = {
@@ -15,6 +20,33 @@ class App extends Component {
       .catch(err => console.log(err));
   };
 
+  handleConnectClick = () => {
+    var socket = new SockJS('/gs-guide-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        
+        document.getElementById("Connect").disabled = true;
+        document.getElementById("Disconnect").disabled = false;
+        //console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (greeting) {
+            serverMessage = JSON.parse(greeting.body).content;
+        });
+    });
+  };
+
+  handleDisconnectClick = () => {
+    if (stompClient !== null) {
+      stompClient.disconnect();
+  }
+  document.getElementById("Connect").disabled = true;
+  document.getElementById("Disconnect").disabled = false;
+  console.log("Disconnected");
+  };
+
+  handleSendName = () => {
+    stompClient.send("/app/hello", {}, JSON.stringify(document.getElementById('Name').value));
+  };
+
   render() {
     return (
       <div className="App">
@@ -24,10 +56,19 @@ class App extends Component {
         <main>
           <button onClick={this.handleClick}>Fetch Me Data!</button>
           The server returned: {JSON.stringify(this.state.response) || ''}
+
+          <button id="Connect" onClick={this.handleConnectClick}>Connect!</button>
+          <button id="Disconnect" disabled="diabled" onClick={this.handleDisconnectClick}>Disconnect!</button>
+          <input id="Name" type ="text" placeholder = "Brian" />
+
+          <button onClick={this.handleSendName}>Send!</button>
+          The server says: {this.serverMessage}
+
         </main>
       </div>
     );
   }
 }
+
 
 export default App;
