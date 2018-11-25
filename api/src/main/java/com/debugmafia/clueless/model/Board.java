6d1732275a6fed68186906f9fd058e9378b5f81e
greenca6 <greenca6@gmail.com>
2018-11-25 17:09:36 -0500

    Remove unused import

--- api/src/main/java/com/debugmafia/clueless/model/Board.java
+++ api/src/main/java/com/debugmafia/clueless/model/Board.java
@@ -7,7 +7,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Board {
   private List<BoardLocation> grid;
